08bdb1534511d4357723422958e889f3e7c1f794
Gobleizer <goble.kevin@gmail.com>
2024-08-04 00:00:14 -0700

    perf: reduces unnecessary sorting per directory
    Listing will only cause a sort to be initiated if this particular directory is unsorted
    Directories can only become unsorted if more than one child directory has been added

--- src/directoryClass.js
+++ src/directoryClass.js
@@ -2,10 +2,14 @@ export default class Directory {
   constructor (name) {
     this.name = name
     this.children = new Map([])
+    this.isChildrenSorted = true
   }
 
   addChild (child) {
     this.children.set(child.name, child)
+    if (this.children.size > 1) {
+      this.isChildrenSorted = false
+    }
   }
 
   deleteChild (child) {
@@ -24,7 +28,15 @@ export default class Directory {
     return this.children.get(childName)
   }
 
+  sortChildren () {
+    this.children = new Map([...this.children].sort(([a], [b]) => String(a).localeCompare(b)))
+    this.isChildrenSorted = true
+  }
+
   getChildrenNamesAlphabetically () {
-    return this.children.keys().toArray().sort()
+    if (!this.isChildrenSorted) {
+      this.sortChildren()
+    }
+    return this.children.keys().toArray()
   }
 }
